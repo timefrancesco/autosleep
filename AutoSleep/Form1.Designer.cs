37cab5f465a7fd27447f01b2cb4b374c6a17fbb7
Francesco Pretelli <fpretelli@gmail.com>
2014-11-22 11:55:43 +1300

    first commit

---
+++ AutoSleep/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AutoSleep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SecsTbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.mynotifyicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SecsTbox
+            // 
+            this.SecsTbox.Location = new System.Drawing.Point(35, 48);
+            this.SecsTbox.Name = "SecsTbox";
+            this.SecsTbox.Size = new System.Drawing.Size(100, 20);
+            this.SecsTbox.TabIndex = 1;
+            this.SecsTbox.Text = "60";
+            this.SecsTbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Interval in minutes:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 117);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // mynotifyicon
+            // 
+            this.mynotifyicon.Icon = ((System.Drawing.Icon)(resources.GetObject("mynotifyicon.Icon")));
+            this.mynotifyicon.Text = "Auto Sleep";
+            this.mynotifyicon.Visible = true;
+            this.mynotifyicon.Click += new System.EventHandler(this.mynotifyicon_Click);
+            this.mynotifyicon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mynotifyicon_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(168, 142);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SecsTbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sleep";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox SecsTbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NotifyIcon mynotifyicon;
+    }
+}
+
---
+++ AutoSleep/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace AutoSleep
+{
+    public partial class Form1 : Form
+    {
+        int _intervalInSecs;
+        Timer _checkInputIntervalTimer;
+        bool _started = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SystemEvents.PowerModeChanged += OnPowerChange;
+            Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!_started)
+                Start();
+            else
+                Stop();
+
+   
+        }
+
+        /// <summary>
+        /// Start The Timer that checks if there has been any input
+        /// </summary>
+        private void Start()
+        {
+            if (SecsTbox.Text != "")
+            {
+                int intervalInMinutes;
+                if (Int32.TryParse(SecsTbox.Text, out intervalInMinutes))
+                {
+                    _intervalInSecs = intervalInMinutes * 60;
+                    if (_checkInputIntervalTimer == null)
+                        _checkInputIntervalTimer = new Timer();
+                    _checkInputIntervalTimer.Interval = 1000; //checks every seconds
+                    _checkInputIntervalTimer.Tick += checkInput_Tick;
+                    _checkInputIntervalTimer.Start();
+                    _started = true;
+                    button1.Text = "Stop";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stops the timer
+        /// </summary>
+        private void Stop()
+        {
+            if (_checkInputIntervalTimer != null)
+            {
+                _checkInputIntervalTimer.Stop();
+                _checkInputIntervalTimer.Tick -= checkInput_Tick;
+            }
+            _started = false;
+            button1.Text = "Start";
+        }
+
+        void checkInput_Tick(object sender, EventArgs e)
+        {
+
+            var lastInput = IdleTimeFinder.GetLastInputTime(); //last input in milliseconds
+
+
+            if (lastInput > _intervalInSecs*1000) 
+            {
+                Stop();
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+            }
+        }
+
+  
+
+        private void OnPowerChange(object s, PowerModeChangedEventArgs e) 
+        {
+            switch ( e.Mode ) 
+            {
+                case PowerModes.Resume:
+                    Console.WriteLine("RESUME");
+                    Timer delayStartTimer = new Timer();
+                    delayStartTimer.Interval = 60000;
+                    delayStartTimer.Tick += delayStartTimer_Tick;
+                    delayStartTimer.Start();
+                break;
+                case PowerModes.Suspend:
+
+                Console.WriteLine("SUSPEND");
+                break;
+            }
+        }
+
+        /// <summary>
+        /// After resume, the timer is restarted after a minute
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void delayStartTimer_Tick(object sender, System.EventArgs e)
+        {
+            ((Timer)sender).Stop();
+            ((Timer)sender).Tick -= delayStartTimer_Tick;
+            Console.WriteLine("RESTART TIMER");
+            Start();
+        }
+      
+
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+
+            public uint dwTime;
+        }
+
+        public class IdleTimeFinder
+        {
+            [DllImport("User32.dll")]
+            private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+            [DllImport("Kernel32.dll")]
+            private static extern uint GetLastError();
+
+            public static uint GetIdleTime()
+            {
+                LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+                lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+                GetLastInputInfo(ref lastInPut);
+
+                return ((uint)Environment.TickCount - lastInPut.dwTime);
+            }
+            /// <summary>
+            /// Get the Last input time in ticks
+            /// </summary>
+            /// <returns></returns>
+            public static uint GetLastInputTime()
+            {
+                LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+                lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+                if (!GetLastInputInfo(ref lastInPut))
+                {
+                    throw new Exception(GetLastError().ToString());
+                }
+                return ((uint)Environment.TickCount - lastInPut.dwTime);
+            }
+
+          
+
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                mynotifyicon.Visible = true;
+             //   mynotifyicon.ShowBalloonTip(500);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                mynotifyicon.Visible = false;
+            }
+        }
+
+        private void mynotifyicon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+          
+        }
+
+        private void mynotifyicon_Click(object sender, System.EventArgs e)
+        {
+
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
